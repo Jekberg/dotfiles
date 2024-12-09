0704318e558a116288e8821b2f4bf3c8fd475afa
John Berg <john.berg.96bits@gmail.com>
2024-12-09 23:07:22 +0000

    Retry when fetcing information via a network
    As is the case for the updates and weather modules. It seems these are
    prone to fail on startup because they might run before networking has
    been established. So we just retry a few times as we might get connected
    later.
    
    Signed-off-by: John Berg <john.berg.96bits@gmail.com>

--- home/.config/waybar/scripts/weather-report.py
+++ home/.config/waybar/scripts/weather-report.py
@@ -6,14 +6,24 @@ Fetch the current weather report for displaying in waybar.
 
 import requests
 import json
+import time
 
 def main():
-    output = {
-        'text': fetch_report(1).strip(),
-        'tooltip': fetch_report(4).strip()
-    }
-    print(json.dumps(output, indent=4))
-
+    max_attempts = 10
+    attempt = 0
+    while True:
+        try:
+            output = {
+                'text': fetch_report(1).strip(),
+                'tooltip': fetch_report(4).strip()
+            }
+            print(json.dumps(output, indent=4))
+            break
+        except Exception as e:
+            attempt += 1
+            if attempt > max_attempts:
+                raise e
+            time.sleep(2 * attempt)
 def fetch_report(format):
     assert type(format) == int
     url = 'https://wttr.in/?format={}'.format(format)
