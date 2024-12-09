5a4c064cba2536ce2d23677e5e43b92dd8923961
John Berg <john.berg.96bits@gmail.com>
2024-12-09 22:56:10 +0000

    Add a basic bash script to help me sync changes before commiting
    It takes a bit of tweaking until I am happy with the end result of my
    configurations. This patch just adds a utility which will sync the files
    in the repo with the local files, which will make it simpler to capture
    changes. For now, just do this for the home directory.
    
    Signed-off-by: John Berg <john.berg.96bits@gmail.com>

---
+++ scripts/update.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env sh
+#
+# This is a utility for synching the actual files with the repository.
+# This allows me to experiment with the files until I feel happy with the
+# results, at which point I can sync them and commit them to this repository.
+#
+# Exampels:
+#
+# update.sh /path/to/file
+# update.sh /path/to/dir
+
+set -o errexit -o nounset -o pipefail
+
+TARGET_PATH=$(realpath $1)
+
+case $TARGET_PATH in
+"${HOME}/"*)
+    LOCAL_PATH="$(dirname home/${TARGET_PATH#${HOME}/})/"
+    echo rsync -avA --delete "${TARGET_PATH}" "${LOCAL_PATH}"
+esac
