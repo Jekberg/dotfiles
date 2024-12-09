99a9b2092b6faf0e02c3df2c8b299418a6ebf66a
John Berg <john.berg.96bits@gmail.com>
2024-12-09 23:07:22 +0000

    Retry when fetcing information via a network
    As is the case for the updates and weather modules. It seems these are
    prone to fail on startup because they might run before networking has
    been established. So we just retry a few times as we might get connected
    later.
    
    Signed-off-by: John Berg <john.berg.96bits@gmail.com>

--- home/.config/waybar/scripts/check-updates.py
+++ home/.config/waybar/scripts/check-updates.py
@@ -9,8 +9,23 @@ provides a complete list as a tooltip.
 
 import json
 import subprocess
+import time
 
 def main():
+    max_attempts = 10
+    attempt = 0
+    while True:
+        try:
+            output = check_updates()
+            print(json.dumps(output, indent=4))
+            break
+        except Exception as e:
+            attempt += 1
+            if attempt > max_attempts:
+                raise e
+            time.sleep(2 * attempt)
+
+def check_updates():
     checkupdates_result = subprocess.run(['checkupdates'],
                                          stdout=subprocess.PIPE)
     try:
@@ -18,24 +33,23 @@ def main():
 
         pending_updates = checkupdates_result.stdout.decode('UTF-8')
         nr_pending_updates = len(pending_updates.splitlines())
-        output = {
+        return {
             'text'   : nr_pending_updates,
             'tooltip': pending_updates,
             'class'  : 'pending-updates' if nr_pending_updates else 'no-updates'
         }
-        print(json.dumps(output, indent=4))
     except Exception as e:
         if checkupdates_result.returncode != 2:
             # checkupdates will inidcate that there are no updates available by
             # by returning an error code of 2. So anything else, we consider a
             # failure.
             raise e
-        output = {
+        return {
             'text'   : 0,
             'tooltip': 'No updates',
             'class'  : 'no-updates'
         }
-        print(json.dumps(output, indent=4))
+
 
 if __name__ == '__main__':
     main()
