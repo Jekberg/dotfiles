60c40ddc0257ed6fc4656cffc66296b3e4319350
John Berg <john.berg.96bits@gmail.com>
2025-01-04 19:02:20 +0000

    Log to stderr instead of stdout during install
    This just seems to be better as it allows us to separate logs out from
    meaningful output.
    
    Signed-off-by: John Berg <john.berg.96bits@gmail.com>

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,23 +11,23 @@ if ! pacman -Qi yay &> /dev/null
 then
     # Not all package dependencies are official arch packages. Some comes from
     # the AUR. Install a AUR helper to make it convinient to install packages.
-    echo "Please install yay"
+    echo "Please install yay" > /dev/stderr
     exit 0
 fi
 
 echo "Installing packages!"
 MISSING="$(grep -vxF -f <(pacman -Qq) packages.txt || echo)"
-echo $MISSING
+echo $MISSING > /dev/stderr
 if [[ -n "${MISSING}" ]]
 then
     # Some packages are from the AUR so we can install everything using yay
     yay -S ${MISSING}
 fi
 
-echo "Installing home files!"
+echo "Installing home files!" > /dev/stderr
 HOME_FILES=$(find  home/{.config,.local}/ -mindepth 1 -maxdepth 1 -type d,f)
 for LOCAL_PATH in $HOME_FILES
 do
     TARGET_PATH="$(dirname $HOME/${LOCAL_PATH#home/})"
-    cp -vr "${LOCAL_PATH}" "${TARGET_PATH}"
+    cp -vr "${LOCAL_PATH}" "${TARGET_PATH}" > /dev/stderr
 done
