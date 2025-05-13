d3cac87fb102eb8f85cd5411553da290243a3747
John Berg <john.berg.96bits@gmail.com>
2025-05-13 19:24:26 +0100

    Remove the nwg-hyprland-dock
    I initially liked it, but afrer tweaking some more it just looks a bit
    strange and out of place as it takes up some screen space. And a
    non-exclusive mode I also don't like.
    
    Signed-off-by: John Berg <john.berg.96bits@gmail.com>

--- home/.config/hypr/scripts/reload-dock.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-
-set -eu
-pkill -f nwg-dock-hyprland || true
-
-# Reloading seem to fail if we don't wait a bit after shuttinf down the
-# previous instance.
-sleep 0.1 && nwg-dock-hyprland -x -i 38 -mt 1 -mb 8 -iw special -c "${HOME}/.local/bin/app-launcher.sh" &
