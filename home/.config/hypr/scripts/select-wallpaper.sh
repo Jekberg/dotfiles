56aab802fddb9a08026f7e1a481b5dcc593476a1
John Berg <john.berg.96bits@gmail.com>
2025-06-04 23:43:20 +0100

    Store the last selected wallpaper in .cache
    This means that later we can recollect which wallpaper was set by
    reading this file. Also, fix a small typo in a comment.
    
    Signed-off-by: John Berg <john.berg.96bits@gmail.com>

--- home/.config/hypr/scripts/select-wallpaper.sh
+++ home/.config/hypr/scripts/select-wallpaper.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 #
-# This is just a small urility for selecting wallpapers, nothing fancy.
+# This is just a small utility for selecting wallpapers, nothing fancy.
 # Wallpapers will go into the $WALLPAPER_DIR which is scanned by this
 # script, making it quick and easy to set the wallpaper.
 
@@ -8,6 +8,9 @@ set -o errexit -o nounset -o pipefail
 
 WALLPAPERS_DIR="${HOME}/.local/wallpapers"
 WALLPAPERS=$(find "${WALLPAPERS_DIR}/"  -type f | xargs -n1 basename)
-WALLPAPER=$(echo "${WALLPAPERS}" | rofi -dmenu -p "Select wallpaper")
+WALLPAPER_FILE=$(echo "${WALLPAPERS}" | rofi -dmenu -p "Select wallpaper")
 
-hyprctl hyprpaper reload ,"${WALLPAPERS_DIR}/${WALLPAPER}"
+WALLPAPER_PATH="${WALLPAPERS_DIR}/${WALLPAPER_FILE}"
+
+hyprctl hyprpaper reload ,"${WALLPAPER_PATH}" &&
+    echo "${WALLPAPER_PATH}" > ~/.cache/selected_wallpaper
