2a3d19945de3e0ee355b10de46572deaac40cd10
Jekberg <john.berg.96bits@gmail.com>
2025-09-15 00:21:47 +0100

    Install the lazy.nvim plugin manager
    This is for easy of installing plugins in NeoVim. This seems like a good
    starting point as lazy appears to be a popular choice. Maybe I will
    change my mind in the future. A low hanging fruit is to get syntax
    highlighting going on the gentoo files. This is aleady documented for
    lazy, as such lets grab the plugin for Gentoo!
    
    Signed-off-by: Jekberg <john.berg.96bits@gmail.com>

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -12,9 +12,12 @@
  |_____\___/_/   \_(_)___|_| \_|___| |_|
 ]]--
 
+require("config.lazy")
+
 vim.opt.expandtab   = true
 vim.opt.tabstop     = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth  = 4
 
 vim.cmd("highlight Normal ctermbg=none guibg=none")
+
