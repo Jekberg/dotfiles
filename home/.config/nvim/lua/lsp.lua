803c961fb593567bc87a708bdc05d1859901a6c4
John Berg <john.berg.96bits@gmail.com>
2025-10-04 17:17:25 +0100

    Small rework to lsp.lua
    Just putting some new syntax I learned into practice. Mainly defining
    the ``lsp_config`` using syntax which is similar to designated
    initialisers.
    
    Signed-off-by: John Berg <john.berg.96bits@gmail.com>

--- home/.config/nvim/lua/lsp.lua
+++ home/.config/nvim/lua/lsp.lua
@@ -1,38 +1,36 @@
-local lsp_config = {}
-
-lsp_config['luals'] = {
-  cmd = { 'lua-language-server' },
-  filetypes = { 'lua' },
-  root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
-  settings = {
-    Lua = {
-      diagnostics = {
-          globals = { 'vim' },
-      },
-      runtime = {
-        version = 'LuaJIT',
-      }
-    }
-  }
-}
-
-lsp_config['clangd'] = {
-    cmd = { 'clangd' },
-    filetypes = { 'c','cc', 'cxx', 'cpp', 'h', 'hh', 'hxx', 'hpp' , 's'},
-    root_markers = { '.clangd', 'compile_command.json' },
-    capabilities = {
-        textDocument = {
-            semanticTokens = {
-                multilineTokenSupport = true,
+local lsp_config = {
+    clangd = {
+        cmd = { 'clangd' },
+        filetypes = { 'c','cc', 'cxx', 'cpp', 'h', 'hh', 'hxx', 'hpp' , 's'},
+        root_markers = { '.clangd', 'compile_command.json' },
+        capabilities = {
+            textDocument = {
+                semanticTokens = {
+                    multilineTokenSupport = true,
+                }
             }
         }
-    }
-}
-
-lsp_config['rust-analyzer'] = {
-    cmd = { 'rust-analyzer' },
-    filetypes = { 'rust' },
-    root_markers = { 'Cargo.toml', 'Cargo.lock' }
+    },
+    lulals = {
+        cmd = { 'lua-language-server' },
+        filetypes = { 'lua' },
+        root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
+        settings = {
+            Lua = {
+                diagnostics = {
+                    globals = { 'vim' },
+                },
+                runtime = {
+                    version = 'LuaJIT',
+                }
+            }
+        }
+    },
+    rust_analyzer = {
+        cmd = { 'rust-analyzer' },
+        filetypes = { 'rust' },
+        root_markers = { 'Cargo.toml', 'Cargo.lock' }
+    },
 }
 
 for k,v in pairs(lsp_config) do
