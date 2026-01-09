204bed4458327c3cdcb113b9fd5ec4b86a0674fe
John Berg <john.berg.96bits@gmail.com>
2026-01-09 00:33:21 +0000

    Add scripts for detecting appropriate videocards use flags
    This patch aim to make it easier to detect which video cards are present
    on a system. And automatically generate the use flag file. The video
    cards recognised is currently limited to the RX 7900, but this list
    should grow when I know which use flags correspond to what GPUs.
    
    Signed-off-by: John Berg <john.berg.96bits@gmail.com>

---
+++ scripts/gentoo/detect-videocards.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#
+# This is a simple script which aims to detect which video card use flags
+# should be used. A system may have more than a single graphics card, and
+# some cards require multiple use flags to be selected.
+
+hw_to_videocards() {
+    local description=$(cat /dev/stdin)
+    case $description in
+        "Navi 31 [Radeon RX 7900 XT/7900 XTX/7900 GRE/7900M]")
+            echo "amdgpu"
+            echo "radeonsi"
+            ;;
+        *)
+            # We have not seen this video card before
+            echo "Unknown video card: $description" > /dev/stderr
+            exit 1
+            ;;
+    esac
+}
+
+lshw -class display -json 2> /dev/null |  jq -r '.[].product' | hw_to_videocards | uniq
